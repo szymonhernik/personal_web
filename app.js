ae97170e744af395001e365a47b3dd0ee62748ed
Szymon Hernik <szymonhernik123@gmail.com>
2021-05-04 12:31:31 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const port = 8000
+const path = require('path');
+// const livereload = require('livereload');
+// const connectLivereload = require("connect-livereload");
+var sass = require('node-sass');
+var sassMiddleware = require('node-sass-middleware');
+
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public'),
+  debug: true,
+  indentedSyntax: true
+}));
+
+const publicDirectory = path.join(__dirname, 'public');
+
+
+// const liveReloadServer = livereload.createServer();
+// liveReloadServer.watch(publicDirectory);
+// // liveReloadServer.watch(sassDirectory);
+// liveReloadServer.server.once("connection", () => {
+//   setTimeout(()=> {
+//     liveReloadServer.refresh("/public");
+//     // liveReloadServer.refresh("/sass");
+//   },100)
+// })
+//
+// app.use(connectLivereload())
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
+
+app.use(express.static('public'))
